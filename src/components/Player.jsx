adbbb716fbad21dba595a91a687e31c2400f18b8
Umut Bektaş <umutbektass.78@hotmail.com>
2024-08-12 17:35:49 +0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+    const [playerName,setPlayerName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(wasEditing => !wasEditing); // best practice setIsEditing(!isEditing) yerine bunu kullan. 
+    //Bu daha performanslı ve daha hızlı çalışır.
+  }
+  function handleChange (e) {
+    setPlayerName(e.target.value)
+  }
+  let editablePlayer = <span className="player-name">{playerName}</span>;
+  let btnCaption="Edit"
+  if (isEditing) {
+    editablePlayer = <input type="text" required value={playerName} onChange={handleChange} />;
+    btnCaption = "Save"
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayer}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{btnCaption}</button>
+      </span>
+    </li>
+  );
+};
+
+export default Player;
